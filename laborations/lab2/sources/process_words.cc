a548d71e65406825513c78443fc847e7e18460e9
wribbe <eng_steff@hotmail.com>
2017-02-07 09:42:41 +0100

    create_ngrams(string, int) returns vector of ngrams.

--- laborations/lab2/sources/process_words.cc
+++ laborations/lab2/sources/process_words.cc
@@ -1,7 +1,29 @@
-#include <fstream>
-#include <iostream>
-#include <istream>
-#include <string>
+#include <algorithm>    // reverse.
+#include <fstream>      // ifstream/ofstream.
+#include <iostream>     // cin/cout/cerr.
+#include <istream>      //
+#include <string>       // string.
+#include <vector>       // vector.
+
+std::vector<std::string> make_ngrams(std::string string, size_t ngram_size)
+    /* If the string is shorter or equal to ngram_size chars, return the
+     * string. If the string is longer then ngram_size chars, return a sorted
+     * vector of the n-grams. */
+{
+    std::vector<std::string> return_vector;
+    if (string.length() <= ngram_size) {
+        return_vector.push_back(string);
+        return return_vector;
+    }
+    for (size_t index=ngram_size-1; index<string.length(); ++index) {
+        return_vector.push_back(string.substr(index-(ngram_size-1), ngram_size));
+    }
+    // Sort the return vector.
+    std::sort(return_vector.begin(), return_vector.end());
+    // Return sorted vector.
+    return return_vector;
+}
+
 
 int main(int argc, char * argv[])
     /* Process a input file at a given path by the command line arguments.
@@ -26,6 +48,9 @@ int main(int argc, char * argv[])
     // Process and print all the lines in the file.
     std::string line = "";
     while (std::getline(input_file, line)) {
-        std::cout << line << std::endl;
+        for (std::string string : make_ngrams(line, 3)) {
+            std::cout << string << " ";
+        }
+        std::cout << std::endl;
     }
 }
