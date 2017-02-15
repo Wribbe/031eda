5f4ddec312da7c67e0b8279e91b917563f4df928
wribbe <eng_steff@hotmail.com>
2017-02-15 09:04:41 +0100

    Change order of declarations of member functions/data in Dictionary.

--- laborations/lab2/includes/dictionary.h
+++ laborations/lab2/includes/dictionary.h
@@ -20,13 +20,17 @@ public:
 	bool contains(const std::string& word) const;
 	std::vector<std::string> get_suggestions(const std::string& word) const;
 private:
+    /* Data. */
     std::unordered_set<std::string> internal_set;
     std::string input_path;
-    void strings_to_internal_set(std::vector<std::string> input_strings);
     static const size_t max_word_length = 25;
     std::vector<Word> words[max_word_length];
+    int sort_matrix[max_word_length+1][max_word_length+1];
+    /* Functions. */
+    void strings_to_internal_set(std::vector<std::string> input_strings);
     void add_trigram_suggestions(std::vector<std::string>& suggestions,
                                  const std::string& word) const;
+    void sort_candidates(std::vector<std::string> suggestions);
 };
 
 #endif
