4104cb3fc9aeb104d92ac46cee892a81bf3fa423
wribbe <eng_steff@hotmail.com>
2017-02-15 15:46:49 +0100

    Extend TagRemover, returns unparsed input string.

--- laborations/lab3/lib/tag_remover.cc
+++ laborations/lab3/lib/tag_remover.cc
@@ -1,2 +1,32 @@
-class TagRemover {
-};
+#include "tag_remover.h"
+
+/* Constructors. */
+TagRemover::TagRemover(std::istream& cin)
+    /* Take data from cin, convert to string and parse. */
+{
+}
+
+TagRemover::TagRemover(const std::string& input)
+    /* Take input string and parse. */
+{
+    parse_string(input);
+}
+
+/* Public functions. */
+
+void TagRemover::print(std::ostream& cout)
+    /* Print output to ostream. */
+{
+    cout << this->parse_results << std::endl;
+}
+
+/* Private functions. */
+void TagRemover::parse_string(const std::string& input)
+    /* Take a input string parse it and return parsed string. */
+{
+    std::stringstream string_buffer;
+    for (char c : input) {
+        string_buffer << c;
+    }
+    this->parse_results = string_buffer.str();
+}
--- laborations/lab3/lib/test_A1.cc
+++ laborations/lab3/lib/test_A1.cc
@@ -1,8 +1,14 @@
 #include <iostream>
 
 #include "test.h"
+#include "tag_remover.h"
 
 int main(void)
 {
-    std::cout << "test" << std::endl;
+    print_test_header("TagRemover");
+
+    TagRemover no_change("Test");
+    no_change.print(std::cout);
+
+    print_test_results();
 }
