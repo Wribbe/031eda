5052d6df4a3b99f7e0ff12e630f4e9089e7bab5d
wribbe <eng_steff@hotmail.com>
2017-02-16 09:22:44 +0100

    Re-tab date.h file.

--- laborations/lab3/includes/date.h
+++ laborations/lab3/includes/date.h
@@ -8,23 +8,23 @@
 class Date {
 public:
     /* Constructors. */
-	Date();                    // today's date
-	Date(int y, int m, int d); // yyyy-mm-dd
+    Date();                    // today's date
+    Date(int y, int m, int d); // yyyy-mm-dd
     /* Public functions. */
-	int getYear() const;       // get the year
-	int getMonth() const;      // get the month
-	int getDay() const;        // get the day
-	void next();               // advance to next day
+    int getYear() const;       // get the year
+    int getMonth() const;      // get the month
+    int getDay() const;        // get the day
+    void next();               // advance to next day
     /* Frindes. */
     friend std::istream& operator>>(std::istream& cin, Date& obj);
     friend void operator>>(std::string& input, Date& obj);
 private:
     /* Private data. */
-	int year;  // the year (four digits)
-	int month; // the month (1-12)
-	int day;   // the day (1-..)
+    int year;  // the year (four digits)
+    int month; // the month (1-12)
+    int day;   // the day (1-..)
     static const int num_months = 12;
-	static int daysPerMonth[num_months]; // number of days in each month
+    static int daysPerMonth[num_months]; // number of days in each month
     /* Private functions. */
     bool format_input(std::string& input);
 };
